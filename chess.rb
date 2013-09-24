7a5fbd115d0de243c0f73433918600a1eb051a57
Tommy Nast and Daniel Quan <tommyn@isomedia.com and djquan@gmail.com>
2013-09-24 16:37:31 -0700

    Got valid moves working for slide class.

--- chess.rb
+++ chess.rb
@@ -10,17 +10,6 @@ class Board
       Array.new(8)
     }
     populate_chess_board
-    print_board
-  end
-
-  def print_board
-    board.each do |row|
-      row.each do |piece|
-        puts piece.class
-        print piece.position if piece
-        puts " "
-      end
-    end
   end
 
   def check?(color)
@@ -30,12 +19,9 @@ class Board
       row.each do |piece|
         next unless piece
         next if piece.color == color
-        p piece
-        print piece.move_locations
         return true if piece.move_locations.include?(king_pos)
       end
     end
-    false
   end
 
   private
@@ -50,18 +36,18 @@ class Board
   end
 
   def populate_chess_board
-    8.times { |index|  board[1][index] = Pawn.new(:black,nil, [1,index]) }
-    8.times { |index|  board[6][index] = Pawn.new(:white,nil, [6,index]) }
+    8.times { |index|  board[1][index] = Pawn.new(:black,board, [1,index]) }
+    8.times { |index|  board[6][index] = Pawn.new(:white,board, [6,index]) }
    # board[2][1] = King.new(:white, nil, [2,1])
     [0,7].each do |row|
       color = (row == 0) ? :black : :white
       (0..7).each do |column|
         board[row][column] = case column
-                             when 4 then King.new(color,nil,[row,column])
-                             when 3 then Queen.new(color,nil,[row,column])
-                             when 0, 7 then Rook.new(color,nil,[row,column])
-                             when 1, 6 then Knight.new(color,nil,[row,column])
-                             when 2, 5 then Bishop.new(color,nil,[row,column])
+                             when 4 then King.new(color,board,[row,column])
+                             when 3 then Queen.new(color,board,[row,column])
+                             when 0, 7 then Rook.new(color,board,[row,column])
+                             when 1, 6 then Knight.new(color,board,[row,column])
+                             when 2, 5 then Bishop.new(color,board,[row,column])
                              end
       end
     end
@@ -69,5 +55,30 @@ class Board
 end
  #opp_color == :white ? :black : :white
 b = Board.new
-p b.check?(:white)
+
+# r = Rook.new(:black, b.board, [0,0])
+# p r.move_locations
+#
+# pawn = Pawn.new(:black, b.board, [1,0])
+# p pawn.move_locations
+
+queen = Queen.new(:black, b.board, [2,3])
+p queen.move_locations
+
+# bishop = Bishop.new(:black, b.board, [2,3])
+# p bishop.move_locations
+
+
+
+
+# def print_board
+#   board.each do |row|
+#     row.each do |piece|
+#       puts piece.class
+#       print piece.position if piece
+#       puts " "
+#     end
+#   end
+# end
+
 
--- sliding_pieces.rb
+++ sliding_pieces.rb
@@ -1,5 +1,13 @@
 class SlidingPiece < Piece
-  def valid_moves
+  def valid_move?(pos)
+    if board[pos[0]][pos[1]]
+      if self.color == board[pos[0]][pos[1]].color
+        false
+      end
+    else
+      puts board[pos[0][pos[1]]]
+      true
+    end
   end
 end
 
@@ -7,7 +15,21 @@ class Rook < SlidingPiece
   def move_locations
     move_locations = []
     (0..7).each do |index|
-      move_locations << [index, position[1]] << [position[0], index]
+      #if board[index,position[index]] == PIECE of any kind
+      #Break
+      if valid_move?([index, position[1]])
+        move_locations << [index, position[1]]
+      else
+        break
+      end
+    end
+
+    (0..7).each do |index|
+      if valid_move?([position[0], index])
+        move_locations << [position[0], index]
+      else
+        break
+      end
     end
     move_locations.delete(position)
     move_locations
@@ -20,11 +42,39 @@ class Bishop <  SlidingPiece
     x = position[1]
     y = position[0]
 
+    (1..7).each do |index|
+      if valid_move?([y+index, x + index])
+        move_locations << [y+index, x + index]
+      else
+        break
+      end
+    end
 
     (1..7).each do |index|
-      move_locations << [y+index, x + index] << [y+index, x - index]
-      move_locations << [y-index, y + index] << [y-index, y - index]
+      if valid_move?([y-index, x + index])
+        move_locations <<  [y-index, x + index]
+      else
+        break
+      end
     end
+
+    (1..7).each do |index|
+      if valid_move?([y+index, x - index])
+        move_locations <<  [y+index, x - index]
+      else
+        break
+      end
+    end
+
+    (1..7).each do |index|
+      if valid_move?([y-index, x-index])
+        move_locations <<  [y-index, x - index]
+      else
+        break
+      end
+    end
+
+
     move_locations.select{|position| position.all?{|num| num >= 0 && num < 8}}
   end
 end
@@ -32,8 +82,8 @@ end
 class Queen < SlidingPiece
   def move_locations
     move_locations = []
-    move_locations += Bishop.new(nil, nil, position).move_locations
-    move_locations += Rook.new(nil, nil, position).move_locations
+    move_locations += Bishop.new(nil, board, position).move_locations
+    move_locations += Rook.new(nil, board, position).move_locations
     move_locations
   end
 end
