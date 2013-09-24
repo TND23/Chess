a3905bf0c33376921f978463644b54d44c22cbb1
Tommy Nast and Daniel Quan <tommyn@isomedia.com and djquan@gmail.com>
2013-09-24 14:07:22 -0700

    Refactor chess board population to remove duplication

--- chess.rb
+++ chess.rb
@@ -25,27 +25,18 @@ class Board
     8.times { |index|  board[1][index] = Pawn.new(:black,nil, [1,index]) }
     8.times { |index|  board[6][index] = Pawn.new(:white,nil, [1,index]) }
 
-    board[0][4] = King.new(:black, nil,[0,4])
-    board[7][4] = King.new(:white, nil,[7,4])
-
-    board[0][3] = Queen.new(:black, nil, [0,3])
-    board[7][3] = Queen.new(:white, nil, [7,3])
-
-    board[0][0] = Rook.new(:black, nil,[0,0])
-    board[0][7] = Rook.new(:black, nil,[0,7])
-    board[7][0] = Rook.new(:white, nil,[7,0])
-    board[7][7] = Rook.new(:white, nil,[7,7])
-
-    board[0][1] = Knight.new(:black, nil,[0,1])
-    board[0][6] = Knight.new(:black, nil,[0,6])
-    board[7][1] = Knight.new(:white, nil,[7,1])
-    board[7][6] = Knight.new(:white, nil,[7,6])
-
-    board[0][2] = Bishop.new(:black, nil,[0,2])
-    board[0][5] = Bishop.new(:black, nil,[0,5])
-    board[7][2] = Bishop.new(:white, nil,[7,2])
-    board[7][5] = Bishop.new(:white, nil,[7,5])
-
+    [0,7].each do |row|
+      color = (row == 0) ? :black : :white
+      (0..7).each do |column|
+        board[row][column] = case column
+                             when 4 then King.new(color,nil,[row,column])
+                             when 3 then Queen.new(color,nil,[row,column])
+                             when 0, 7 then Rook.new(color,nil,[row,column])
+                             when 1, 6 then Knight.new(color,nil,[row,column])
+                             when 2, 5 then Bishop.new(color,nil,[row,column])
+                             end
+      end
+    end
   end
 end
 
