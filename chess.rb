dc8d9b2e25f24c96ac0270daa79441269da14cb5
Tommy Nast and Daniel Quan <tommyn@isomedia.com and djquan@gmail.com>
2013-09-24 11:47:20 -0700

    Separate files

--- chess.rb
+++ chess.rb
@@ -1,3 +1,6 @@
+require './sliding_pieces.rb'
+require './pieces.rb'
+
 class Board
   def initialize
     @board = Array.new(8) {
@@ -11,79 +14,3 @@ class Board
   end
 end
 
-class Piece
-  attr_reader :color, :board, :position
-
-  def initialize(color, board, position)
-    @color = color
-    @board = board
-    @position = position
-  end
-end
-
-class SlidingPiece < Piece
-  def valid_moves
-  end
-end
-
-class Rook < SlidingPiece
-  def move_locations
-    move_locations = []
-    (0..7).each do |index|
-      move_locations << [index, position[1]] << [position[0], index]
-    end
-    move_locations.delete(position)
-    move_locations
-  end
-end
-
-
-class Bishop <  SlidingPiece
-
-  def move_locations
-    move_locations = []
-    x = position[1]
-    y = position[0]
-    (1..7).each do |index|
-      move_locations << [x+index, y + index] << [x+index, y - index]
-      move_locations << [x-index, y + index] << [x-index, y - index]
-    end
-    move_locations.select{|position| position.all?{|num| num >= 0 && num < 8}}
-  end
-end
-
-class Queen < SlidingPiece
-
-  def move_locations
-    move_locations = []
-    move_locations += Bishop.new(nil, nil, position).move_locations
-    move_locations += Rook.new(nil, nil, position).move_locations
-    move_locations
-  end
-end
-
-class Pawn < Piece
-  attr_accessor :first_move
-
-  def initialize(color, board, position)
-    super(color,board,position)
-    @first_move = true
-  end
-
-  def move_locations
-    move_locations = []
-    case color
-    when :black
-      move_locations << [position[0] + 1, position[1]]
-      move_locations << [position[0] + 2, position[1]] if first_move
-    when :white
-      move_locations << [position[0] - 1, position[1]]
-      move_locations << [position[0] - 2, position[1]] if first_move
-    end
-    move_locations
-    #Write code to make attacking stuff blah
-  end
-end
-
-q = Queen.new(nil, nil, [4,4])
-p q.move_locations
---
+++ sliding_pieces.rb
@@ -0,0 +1,37 @@
+class SlidingPiece < Piece
+  def valid_moves
+  end
+end
+
+class Rook < SlidingPiece
+  def move_locations
+    move_locations = []
+    (0..7).each do |index|
+      move_locations << [index, position[1]] << [position[0], index]
+    end
+    move_locations.delete(position)
+    move_locations
+  end
+end
+
+class Bishop <  SlidingPiece
+  def move_locations
+    move_locations = []
+    x = position[1]
+    y = position[0]
+    (1..7).each do |index|
+      move_locations << [x+index, y + index] << [x+index, y - index]
+      move_locations << [x-index, y + index] << [x-index, y - index]
+    end
+    move_locations.select{|position| position.all?{|num| num >= 0 && num < 8}}
+  end
+end
+
+class Queen < SlidingPiece
+  def move_locations
+    move_locations = []
+    move_locations += Bishop.new(nil, nil, position).move_locations
+    move_locations += Rook.new(nil, nil, position).move_locations
+    move_locations
+  end
+end
